80747c514cc2f20207f2bdacef7131b3a34ec7c0
Helgochka1205 <theolyainsight@gmail.com>
2025-08-07 19:07:24 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 /* CONFIGURATION STARTS HERE */
 
 /* Step 1: enter your domain name like fruitionsite.com */
-const MY_DOMAIN = "fruitionsite.com";
+const MY_DOMAIN = "olyainsight.com";
 
 /*
  * Step 2: enter your URL slug to page ID mapping
@@ -9,7 +9,7 @@ const MY_DOMAIN = "fruitionsite.com";
  * The value on the right is the Notion page ID
  */
 const SLUG_TO_PAGE = {
-  "": "771ef38657244c27b9389734a9cbff44",
+  "": "234306528551805ca11fd15629e032f8",
   thanks: "9d9864f5338b47b0a7f42e0f0e2bbf46",
   showcase: "92053970e5084019ac096d2df7e7f440",
   roadmap: "7d4b21bfb4534364972e8bf9f68c2c36"
